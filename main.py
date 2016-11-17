fbcb31223b51e0aa2115dc1f7fc09bd9f434e777
raleng <github@ralfengbers.com>
2016-11-17 01:44:18 +0100

    added some comments

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from kivy.app import App
-
 from kivy.uix.boxlayout import BoxLayout
 
 from math import factorial
 
 
 def binomial(n, k):
+    """ Calculates the binomial coefficient (n over k). """
     return factorial(n) / (factorial(k) * factorial(n-k))
 
 
 def calc_odds(num_dice, win_with):
+    """ Computes the probabilities for killing any number of zombies.
+
+    The formula is:
+    (n over k) * (p)^k * (1-p)^(n-k)
+    """
     win_odds = (7 - win_with) / 6
     lose_odds = 1 - win_odds
 
@@ -24,6 +29,7 @@ def calc_odds(num_dice, win_with):
 
 
 def expected_kills(odds):
+    """ Computes the expected value of Zombie Kills. """
     expect = 0
     for c, odd in enumerate(odds, start=1):
         expect += c * odd
@@ -31,9 +37,12 @@ def expected_kills(odds):
 
 
 def odds_list(odds, crawl):
+    """ Generates a string, listing the cumulative probabilities for killing any
+    number of zombies.
+    """
     cum_prob = [sum(odds[c:])*100 for c in range(len(odds))]
     cum_prob_crawl = [sum(crawl[c:])*100 for c in range(len(crawl))]
-    cum_prob_crawl.append(0)
+    cum_prob_crawl.append(0)  # last entry is always 0, because all dice kill
 
     odds_str = '{} Zombie: {:.1f}%/{:.1f}%\n'.format(1, cum_prob[0], cum_prob_crawl[0])
     for c, (prob, cr) in enumerate(zip(cum_prob[1:], cum_prob_crawl[1:]), start=2):
@@ -46,6 +55,7 @@ class MainWidget(BoxLayout):
 
     @staticmethod
     def decrease_num(current_num):
+        """ Decreases number by one. Dose not decrease below 1. """
         if int(current_num) > 1:
             return str(int(current_num) - 1)
         else:
@@ -53,25 +63,31 @@ class MainWidget(BoxLayout):
 
     @staticmethod
     def increase_num(current_num):
+        """ Increases number by one. Dose not increase over 6. """
         if int(current_num) < 6:
             return str(int(current_num) + 1)
         else:
             return str(6)
 
     def change_odds(self):
+        """ Sets the text labels according to the current probabilities. """
         labels = [['num_1', 'win_1', 'expected_1', 'odds_1'], 
                   ['num_2', 'win_2', 'expected_2', 'odds_2']]
 
-        for l_num, l_win, l_exp, l_odd in labels: 
+        for l_num, l_win, l_exp, l_odd in labels:
+            # Getting input values and calculating odds
             dice_num = int(self.ids[l_num].text)
             dice_win = int(self.ids[l_win].text)
             dice_results = calc_odds(dice_num, dice_win)
 
+            # If dice win with 1 or better (i.e. always), no crawler is being created.
+            # Calling 'calc_odds(_, 7)' returns list of zeros
             if dice_win == 1:
                 dice_crawler = calc_odds(dice_num-1, 7)
             else:
                 dice_crawler = calc_odds(dice_num-1, 6)
 
+            # Setting output
             self.ids[l_exp].text = 'Expect {:.2f} Kills'.format(expected_kills(dice_results))
             self.ids[l_odd].text = odds_list(dice_results, dice_crawler)
 
